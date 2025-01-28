0a5ff170347c9dca8b713efe451fbde028d6489a
imrulhak <emonmr768@gmail.com>
2025-01-28 19:06:16 +0600

    Todo work

---
+++ Client/src/App.jsx
@@ -0,0 +1,90 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleTask = (e) => {
+    setTask(e.target.value);
+  };
+
+  const gettodos = () => {
+    axios.get("http://localhost:8000/alltodos").then((data) => {
+      setTodos(data.data);
+    });
+  };
+
+  useEffect(() => {
+    gettodos();
+  }, []);
+
+  const handleAdd = () => {
+    axios
+      .post("http://localhost:8000/add", {
+        name: task,
+      })
+      .then((data) => {
+        console.log(data);
+        alert("add successfull");
+        setTask("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handledelete = (id) => {
+    axios.delete(`http://localhost:8000/deletetodo/${id}`).then((data) => {
+      console.log(data);
+      alert("data deleted");
+    });
+  };
+
+  return (
+    <>
+      <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+        <div className="bg-white rounded shadow border-2 p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+          <div className="mb-4">
+            <h1 className="text-grey-darkest">Todo List</h1>
+            <div className="flex mt-4">
+              <input
+                onChange={handleTask}
+                className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                placeholder="Add Todo"
+              />
+              <button
+                onClick={handleAdd}
+                className="flex-no-shrink p-2 border-2 rounded text-teal border-teal"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+          <div>
+            {todos.map((item) => (
+              <div className="flex mb-4 items-center">
+                <ul>
+                  <li>
+                    {item.name}
+                    <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded text-green">
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handledelete(item._id)}
+                      className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red"
+                    >
+                      Delete
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ Server/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+const port = 8000;
+app.use(cors());
+app.use(express.json());
+
+// connect database
+mongoose
+  .connect(
+    "mongodb+srv://crud:crud@cluster0.11dwg.mongodb.net/crud?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("database is connected");
+  });
+
+// model
+const user = mongoose.model("User", { name: String });
+
+// add task
+app.post("/add", (req, res) => {
+  let { name } = req.body;
+  let result = new user({
+    name: name,
+  });
+  result.save();
+  res.status(201).send({ success: true, msg: "add successfully" });
+});
+
+// alltodos get
+app.get("/alltodos", async (req, res) => {
+  let alltodos = await user.find({});
+  res.send(alltodos);
+});
+
+// delete todos
+app.delete("/deletetodo/:id", async (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  let deletetodo = await user.findOneAndDelete({
+    _id: id,
+  });
+  res
+    .status(200)
+    .send({ success: true, msg: "delete success", data: deletetodo });
+});
+
+app.listen(port || 8000, () => {
+  console.log(`server runing in this port ${port}`);
+});
